bcf25ed74c64c3800e566f9c9078f120b3894ded
Israelmath <israel.usp@gmail.com>
2020-07-03 16:28:54 -0300

    Commit final do aplicativo mínimo que calcula o IMC, dadas altura e o peso.

--- lib/main.dart
+++ lib/main.dart
@@ -18,50 +18,35 @@ class _HomeState extends State<Home> {
   TextEditingController _altura_controlador = TextEditingController();
   String _infoText = 'Informe seus dados';
 
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   void _resetData() {
     _peso_controlador.text = '';
     _altura_controlador.text = '';
-    _infoText = 'Informe seus dados';
-  }
 
-  bool _validate(TextEditingController altura_peso, bool altura){
-
-    if (altura_peso == null){
-      if (altura == false){
-        _infoText = 'Você precisa informar o peso';
-        return false;
-      }
-      else{
-        _infoText = 'Você precisa informar a altura';
-        return false;
-      }
-    }else
-      return true;
+    setState(() {
+      _infoText = 'Informe seus dados';
+      _formKey = GlobalKey<FormState>();
+    });
   }
 
   void _calcula_imc() {
     setState(
       () {
-        if (_validate(_peso_controlador, false) && _validate(_altura_controlador, true)){
+        double _peso = double.tryParse(_peso_controlador.text);
+        double _altura = double.tryParse(_altura_controlador.text);
+        double _imc = _peso / (_altura * _altura);
 
-          debugPrint('_peso_controlador: ${_peso_controlador.text}');
-          debugPrint('_altura_controlador: ${_altura_controlador.text}');
-
-          double _peso = double.tryParse(_peso_controlador.text);
-          double _altura = double.tryParse(_altura_controlador.text);
-          double _imc = _peso / (_altura * _altura);
-
-          if (_imc < 18.6) {
-            _infoText = 'Abaixo do peso\nIMC: ${_imc.toStringAsFixed(2)}';
-          } else if (_imc >= 18.6 && _imc < 24.9) {
-            _infoText = 'Dentro do peso ideal\nIMC: ${_imc.toStringAsFixed(2)}';
-          } else if (_imc >= 24.9 && _imc < 34.9) {
-            _infoText = 'Obesidade Grau I\nIMC: ${_imc.toStringAsFixed(2)}';
-          } else if (_imc >= 34.9 && _imc < 39.9) {
-            _infoText = 'Obesidade Grau II\nIMC: ${_imc.toStringAsFixed(2)}';
-          } else {
-            _infoText = 'Obesidade Grau III\nIMC: ${_imc.toStringAsFixed(2)}';
-          }
+        if (_imc < 18.6) {
+          _infoText = 'Abaixo do peso\nIMC: ${_imc.toStringAsFixed(2)}';
+        } else if (_imc >= 18.6 && _imc < 24.9) {
+          _infoText = 'Dentro do peso ideal\nIMC: ${_imc.toStringAsFixed(2)}';
+        } else if (_imc >= 24.9 && _imc < 34.9) {
+          _infoText = 'Obesidade Grau I\nIMC: ${_imc.toStringAsFixed(2)}';
+        } else if (_imc >= 34.9 && _imc < 39.9) {
+          _infoText = 'Obesidade Grau II\nIMC: ${_imc.toStringAsFixed(2)}';
+        } else {
+          _infoText = 'Obesidade Grau III\nIMC: ${_imc.toStringAsFixed(2)}';
         }
       },
     );
@@ -90,64 +75,82 @@ class _HomeState extends State<Home> {
           ],
         ),
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(
-                Icons.person_outline,
-                size: 120.0,
-                color: Colors.orange[500],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0, left: 16, right: 16),
-                child: TextField(
-                  controller: _peso_controlador,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.orange[500]),
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: '83.2',
-                    labelText: 'Peso (Kg)',
-                    labelStyle: TextStyle(fontSize: 24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.person_outline,
+                  size: 120.0,
+                  color: Colors.orange[500],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 16, right: 16),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Insira o peso';
+                      }
+                    },
+                    controller: _peso_controlador,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.orange[500]),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: '83.2',
+                      labelText: 'Peso (Kg)',
+                      labelStyle: TextStyle(fontSize: 24),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0, left: 16, right: 16),
-                child: TextField(
-                  controller: _altura_controlador,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.orange[500]),
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: '170',
-                    labelText: 'Altura (cm)',
-                    labelStyle: TextStyle(fontSize: 24),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 16, right: 16),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Insira a altura';
+                      }
+                    },
+                    controller: _altura_controlador,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.orange[500]),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: '170',
+                      labelText: 'Altura (cm)',
+                      labelStyle: TextStyle(fontSize: 24),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, left: 16, right: 16),
-                child: Container(
-                  height: 50.0,
-                  child: RaisedButton(
-                    onPressed: _calcula_imc,
-                    child: Text('Calcular'),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 16.0, left: 16, right: 16),
+                  child: Container(
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: (){
+                        if (_formKey.currentState.validate()){
+                          _calcula_imc();
+                        }
+                      },
+                      child: Text('Calcular'),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0, left: 16, right: 16),
-                child: Text(
-                  _infoText,
-                  style: TextStyle(
-                    color: Colors.orange[500],
-                    fontSize: 24,
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 16, right: 16),
+                  child: Text(
+                    _infoText,
+                    style: TextStyle(
+                      color: Colors.orange[500],
+                      fontSize: 24,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
